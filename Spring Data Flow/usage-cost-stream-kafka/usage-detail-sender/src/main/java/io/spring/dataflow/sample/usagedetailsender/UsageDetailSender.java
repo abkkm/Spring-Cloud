250e388cfb498c7a04a4979b2156634aa3703f38
Eugenio Garcia San Martin <egsmartin@gmail.com>
2019-12-06 17:01:17 +0100

    .

--- Spring Data Flow/usage-cost-stream-kafka/usage-detail-sender/src/main/java/io/spring/dataflow/sample/usagedetailsender/UsageDetailSender.java
+++ Spring Data Flow/usage-cost-stream-kafka/usage-detail-sender/src/main/java/io/spring/dataflow/sample/usagedetailsender/UsageDetailSender.java
@@ -2,14 +2,14 @@ package io.spring.dataflow.sample.usagedetailsender;
 
 import java.util.Random;
 
-import io.spring.dataflow.sample.domain.UsageDetail;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import io.spring.dataflow.sample.domain.UsageDetail;
 
 @EnableScheduling
 @EnableBinding(Source.class)
@@ -18,14 +18,15 @@ public class UsageDetailSender {
 	@Autowired
 	private Source source;
 
-	private String[] users = {"Glenn", "Sabby", "Mark", "Janne", "Ilaya"};
+	private final String[] users = {"Glenn", "Sabby", "Mark", "Janne", "Ilaya"};
 
 	@Scheduled(fixedDelay = 1000)
 	public void sendEvents() {
-		UsageDetail usageDetail = new UsageDetail();
+		final UsageDetail usageDetail = new UsageDetail();
 		usageDetail.setUserId(this.users[new Random().nextInt(5)]);
 		usageDetail.setDuration(new Random().nextInt(300));
 		usageDetail.setData(new Random().nextInt(700));
+
 		this.source.output().send(MessageBuilder.withPayload(usageDetail).build());
 	}
 }
